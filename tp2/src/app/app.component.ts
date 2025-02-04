7eb66003dc2f800f0ddd59eff1b594a18aa7d58b
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-02-04 16:53:40 +0100

    premier test de card

--- tp2/src/app/app.component.ts
+++ tp2/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { Section1Component } from "./section1/section1.component";
+import { CardComponent } from "./card/card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, Section1Component],
+  imports: [RouterOutlet, HeaderComponent, Section1Component, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
