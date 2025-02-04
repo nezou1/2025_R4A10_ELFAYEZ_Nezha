8e893f4f21b4c9a84c3be2ae68f2e1bd8e60be7e
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-02-04 16:38:56 +0100

    nav & section hero

--- tp2/src/app/app.component.ts
+++ tp2/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { Section1Component } from "./section1/section1.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, Section1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
