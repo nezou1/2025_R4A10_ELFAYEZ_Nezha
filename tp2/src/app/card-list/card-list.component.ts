580aadf8fa796d70f7ffb4e5927f974ff5c14fae
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-02-15 16:54:37 +0100

    button component

--- tp2/src/app/card-list/card-list.component.ts
+++ tp2/src/app/card-list/card-list.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CardComponent } from "../card/card.component";
 
+/* Définition correcte du type des cartes */
+type Card = {
+  title: string;
+  description: string;
+  imageUrl: string;
+};
+
 @Component({
   selector: 'app-card-list',
+  standalone: true, // Permet une utilisation sans module
   imports: [CardComponent],
   templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.scss'
+  styleUrls: ['./card-list.component.scss']
 })
 export class CardListComponent {
-  @Input() cards: { title: string, description: string, imageUrl: string }[] = [
+  @Input() cards: Card[] = [
     { title: 'Search doctor', description: 'Choose your doctor from thousands of specialists.', imageUrl: 'assets/loop-icon.png' },
     { title: 'Online pharmacy', description: 'Buy your medicines with our mobile app.', imageUrl: 'assets/pharmacy-icon.png' },
     { title: 'Consultation', description: 'Free consultation with our trusted doctors.', imageUrl: 'assets/consultation-icon.png' },
@@ -16,7 +24,4 @@ export class CardListComponent {
     { title: 'Emergency care', description: '24/7 urgent care for you and your family.', imageUrl: 'assets/emergency-icon.png' },
     { title: 'Tracking', description: 'Track and save your medical history.', imageUrl: 'assets/tracking-icon.png' }
   ];
-
-  
-
 }
