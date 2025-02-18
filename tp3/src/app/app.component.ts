cb1a132a51fec20354efa20e9fa825e6cf61f48a
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-02-18 15:57:40 +0100

    test

--- tp3/src/app/app.component.ts
+++ tp3/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {CardComponent} from "./card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
